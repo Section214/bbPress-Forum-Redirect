61ff5a0d92e50b2b8afe26b4c4d9d32777b7c73b
Ghost1227 <dgriffiths@section214.com>
2016-01-20 12:28:44 -0600

    Codebase cleanup

--- bbpress-forum-redirect.php
+++ bbpress-forum-redirect.php
@@ -5,149 +5,119 @@
  * Description:     Allows you to override the default behavior of bbPress forums, linking them to an external site
  * Author:          Daniel J Griffiths
  * Author URI:      http://www.ghost1227.com
- * Version:         1.0.1
+ * Version:         1.1.0
  * Text Domain:     bbpress-forum-redirect
  *
  * @package         bbPress\ForumRedirect
  * @author          Daniel J Griffiths
- * @version         1.0.0
  */
 
 
 // Exit if accessed directly
-if( !defined( 'ABSPATH' ) ) exit;
+if( ! defined( 'ABSPATH' ) ) exit;
 
 
-if( !class_exists( 'bbPress_Forum_Redirect' ) ) {
+if( ! class_exists( 'bbPress_Forum_Redirect' ) ) {
 
-    /**
-     * Main bbPress_Forum_Redirect class
-     *
-     * @since       1.0.0
-     */
-    final class bbPress_Forum_Redirect {
+	/**
+	 * Main bbPress_Forum_Redirect class
+	 *
+	 * @since       1.0.0
+	 */
+	final class bbPress_Forum_Redirect {
 
-        /**
-         * @var         bbPress_Forum_Redirect $instance The one true bbPress_Forum_Redirect
-         * @since       1.0.0
-         */
-        private static $instance;
+		/**
+		 * @var         bbPress_Forum_Redirect $instance The one true bbPress_Forum_Redirect
+		 * @since       1.0.0
+		 */
+		private static $instance;
 
-        /**
-         * Get active instance
-         *
-         * @access      public
-         * @since       1.0.0
-         * @return      object self::$instance The one true bbPress_Forum_Redirect
-         */
-        public static function instance() {
-            if( !isset( self::$instance ) ) {
-                self::$instance = new bbPress_Forum_Redirect;
-                self::$instance->setup_constants();
-                self::$instance->load_textdomain();
-                self::$instance->hooks();
-            }
+		/**
+		 * Get active instance
+		 *
+		 * @access      public
+		 * @since       1.0.0
+		 * @return      object self::$instance The one true bbPress_Forum_Redirect
+		 */
+		public static function instance() {
+			if( ! isset( self::$instance ) ) {
+				self::$instance = new bbPress_Forum_Redirect;
+				self::$instance->setup_constants();
+				self::$instance->load_textdomain();
+				self::$instance->hooks();
+			}
 
-            return self::$instance;
-        }
-
-
-        /**
-         * Setup plugin constants
-         *
-         * @access      private
-         * @since       1.0.0
-         * @return      void
-         */
-        private function setup_constants() {
-            // Plugin version
-            define( 'BBPRESS_FORUM_REDIRECT_VER', '1.0.1' );
-
-            // Plugin folder URL
-            define( 'BBPRESS_FORUM_REDIRECT_URL', plugin_dir_url( __FILE__ ) );
-
-            // Plugin folder dir
-            define( 'BBPRESS_FORUM_REDIRECT_DIR', plugin_dir_path( __FILE__ ) );
-        }
+			return self::$instance;
+		}
 
 
-        /**
-         * Load plugin language files
-         *
-         * @access      public
-         * @since       1.0.0
-         * @return      void
-         */
-        public function load_textdomain() {
-            // Set filter for plugin language directory
-            $lang_dir  = dirname( plugin_basename( __FILE__ ) ) . '/languages/';
-            $lang_dir  = apply_filters( 'bbPress_Forum_Redirect_lang_dir', $lang_dir );
+		/**
+		 * Setup plugin constants
+		 *
+		 * @access      private
+		 * @since       1.0.0
+		 * @return      void
+		 */
+		private function setup_constants() {
+			// Plugin version
+			define( 'BBPRESS_FORUM_REDIRECT_VER', '1.1.0' );
 
-            // WordPress plugin locale filter
-            $locale         = apply_filters( 'plugin_locale', get_locale(), 'bbpress-forum-redirect' );
-            $mofile         = sprintf( '%1$s-%2$s.mo', 'bbpress-forum-redirect', $locale );
+			// Plugin folder URL
+			define( 'BBPRESS_FORUM_REDIRECT_URL', plugin_dir_url( __FILE__ ) );
 
-            // Setup paths
-            $mofile_local   = $lang_dir . $mofile;
-            $mofile_global  = WP_LANG_DIR . '/bbpress-forum-redirect/' . $mofile;
-
-            if( file_exists( $mofile_global ) ) {
-                // Check global wp-content/languages/bbpress-forum-redirect folder
-                load_textdomain( 'bbpress-forum-redirect', $mofile_global );
-            } elseif( file_exists( $mofile_local ) ) {
-                load_textdomain( 'bbpress-forum-redirect', $mofile_local );
-            } else {
-                load_plugin_textdomain( 'bbpress-forum-redirect', false, $lang_dir );
-            }
-        }
+			// Plugin folder dir
+			define( 'BBPRESS_FORUM_REDIRECT_DIR', plugin_dir_path( __FILE__ ) );
+		}
 
 
-        /**
-         * Run action and filter hooks
-         *
-         * @access      private
-         * @since       1.0.0
-         * @return      void
-         */
-        private function hooks() {
-            // Modify plugin metalinks
-            add_filter( 'plugin_row_meta', array( $this, 'plugin_metalinks' ), null, 2 );
+		/**
+		 * Run action and filter hooks
+		 *
+		 * @access      private
+		 * @since       1.0.0
+		 * @return      void
+		 */
+		private function hooks() {
+			// Add metabox to forum post type
+			add_action( 'add_meta_boxes', array( $this, 'add_redirect_metabox' ) );
 
-            // Add metabox to forum post type
-            add_action( 'add_meta_boxes', array( $this, 'add_redirect_metabox' ) );
+			// Save metabox
+			add_action( 'save_post', array( $this, 'save_redirect_metabox' ) );
 
-            // Save metabox
-            add_action( 'save_post', array( $this, 'save_redirect_metabox' ) );
-
-            // Override forum permalinks
-            add_filter( 'bbp_get_forum_permalink', array( $this, 'override_forum_permalink' ), null, 2 );
-        }
+			// Override forum permalinks
+			add_filter( 'bbp_get_forum_permalink', array( $this, 'override_forum_permalink' ), null, 2 );
+		}
 
 
-        /**
-         * Modify plugin metalinks
-         *
-         * @access      public
-         * @since       1.0.0
-         * @param       array $links The current links array
-         * @param       string $file A specific plugin table entry
-         * @return      array $links The modified links array
-         */
-        public function plugin_metalinks( $links, $file ) {
-            if( $file == plugin_basename( __FILE__ ) ) {
-                $help_link = array(
-                    '<a href="http://section214.com/support/forum/bbpress-forum-redirect/" target="_blank">' . __( 'Support Forum', 'edd-balanced-gateway' ) . '</a>'
-                );
+		/**
+		 * Load plugin language files
+		 *
+		 * @access      public
+		 * @since       1.0.0
+		 * @return      void
+		 */
+		public function load_textdomain() {
+			// Set filter for plugin language directory
+			$lang_dir  = dirname( plugin_basename( __FILE__ ) ) . '/languages/';
+			$lang_dir  = apply_filters( 'bbpress_forum_redirect_lang_dir', $lang_dir );
 
-                $docs_link = array(
-                    '<a href="http://section214.com/docs/category/bbpress-forum-redirect/" target="_blank">' . __( 'Docs', 'edd-balanced-gateway' ) . '</a>'
-                );
+			// WordPress plugin locale filter
+			$locale         = apply_filters( 'plugin_locale', get_locale(), 'bbpress-forum-redirect' );
+			$mofile         = sprintf( '%1$s-%2$s.mo', 'bbpress-forum-redirect', $locale );
 
-                $links = array_merge( $links, $help_link, $docs_link );
-            }
+			// Setup paths
+			$mofile_local   = $lang_dir . $mofile;
+			$mofile_global  = WP_LANG_DIR . '/bbpress-forum-redirect/' . $mofile;
 
-            return $links;
-        }
+			if( file_exists( $mofile_global ) ) {
+				// Check global wp-content/languages/bbpress-forum-redirect folder
+				load_textdomain( 'bbpress-forum-redirect', $mofile_global );
+			} elseif( file_exists( $mofile_local ) ) {
+				load_textdomain( 'bbpress-forum-redirect', $mofile_local );
+			} else {
+				load_plugin_textdomain( 'bbpress-forum-redirect', false, $lang_dir );
+			}
+		}
 
 
         /**
@@ -180,7 +150,7 @@ if( !class_exists( 'bbPress_Forum_Redirect' ) ) {
             }
 
             $redirect = isset( $meta['bbpress-forum-redirect'] ) ? esc_attr( $meta['bbpress-forum-redirect'][0] ) : '';
-            
+
             wp_nonce_field( basename( __FILE__ ), 'bbpress_forum_redirect_metabox_nonce' );
 
             echo '<p>' . __( 'Enter a URL to forward this forum to.', 'bbpress-forum-redirect' ) . '</p>';
@@ -245,18 +215,18 @@ if( !class_exists( 'bbPress_Forum_Redirect' ) ) {
  * @since       1.0.0
  * @return      bbPress_Forum_Redirect The one true bbPress_Forum_Redirect
  */
-function bbPress_Forum_Redirect_load() {
-    if( !class_exists( 'bbPress' ) ) {
-        deactivate_plugins( __FILE__ );
-        unset( $_GET['activate'] );
+function bbpress_forum_redirect() {
+	if( ! class_exists( 'bbPress' ) ) {
+		deactivate_plugins( __FILE__ );
+		unset( $_GET['activate'] );
 
-        // Display notice
-        add_action( 'admin_notices', 'bbPress_Forum_Redirect_missing_bbpress_notice' );
-    } else {
-        return bbPress_Forum_Redirect::instance();
-    }
+		// Display notice
+		add_action( 'admin_notices', 'bbpress_forum_redirect_missing_bbpress_notice' );
+	} else {
+		return bbPress_Forum_Redirect::instance();
+	}
 }
-add_action( 'plugins_loaded', 'bbPress_Forum_Redirect_load' );
+add_action( 'plugins_loaded', 'bbpress_forum_redirect_load' );
 
 
 /**
@@ -265,6 +235,6 @@ add_action( 'plugins_loaded', 'bbPress_Forum_Redirect_load' );
  * @since       1.0.1
  * @return      void
  */
-function bbPress_Forum_Redirect_missing_bbpress_notice() {
-    echo '<div class="error"><p>' . __( 'bbPress Forum Redirect requires bbPress! Please install it to continue!', 'bbpress-forum-redirect' ) . '</p></div>';
+function bbpress_forum_redirect_missing_bbpress_notice() {
+	echo '<div class="error"><p>' . __( 'bbPress Forum Redirect requires bbPress! Please install it to continue!', 'bbpress-forum-redirect' ) . '</p></div>';
 }
